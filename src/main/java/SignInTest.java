1a02e72390424342b273a6b95ef2e13df84a6c33
xx <nikky.kumari@entropiktech.com>
2018-10-25 23:40:42 +0530

    updated the chrome driver, added setUp and tearDown method in SignIn Test

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -3,16 +3,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SignInTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+
+    @BeforeMethod
+    private void setUp() {
+        setDriverPath();
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-
-        setDriverPath();
 
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
@@ -36,6 +42,7 @@ public class SignInTest {
     }
 
     private void setDriverPath() {
+        System.out.println(PlatformUtil.isWindows());
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
@@ -47,5 +54,10 @@ public class SignInTest {
         }
     }
 
-
+    @AfterMethod
+    private void tearDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
 }
