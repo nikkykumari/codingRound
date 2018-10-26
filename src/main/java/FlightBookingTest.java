2d015cebfa359cad32a750384a9faff53203bb98
xx <nikky.kumari@entropiktech.com>
2018-10-26 09:56:31 +0530

    done cleanup for FlightBookingTest using PageFactory weblelement identification

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,9 +12,28 @@ import java.util.List;
 
 public class FlightBookingTest extends BaseClass {
 
+    @FindBy(id = "OneWay")
+    private WebElement oneWayButton;
+
+    @FindBy(id = "FromTag")
+    private WebElement fromDropdown;
+
+    @FindBy(id = "ui-id-1")
+    private WebElement fromAutoFill;
+
+    @FindBy(id = "ToTag")
+    private WebElement toDropdown;
+
+    @FindBy(id = "ui-id-2")
+    private WebElement toAutoFill;
+
+    @FindBy(id = "SearchBtn")
+    private WebElement searchButton;
+
     @BeforeMethod
     private void setUp() {
         setDriver();
+        PageFactory.initElements(driver, this);
     }
 
 
@@ -20,34 +42,39 @@ public class FlightBookingTest extends BaseClass {
 
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
-        driver.findElement(By.id("OneWay")).click();
+        oneWayButton.click();
 
-        driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+        fromDropdown.clear();
+        fromDropdown.sendKeys("Bangalore");
 
         //wait for the auto complete options to appear for the origin
 
         waitFor(5000);
-        List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        List<WebElement> originOptions = fromAutoFill.findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("ToTag")).clear();
-        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
+        toDropdown.clear();
+        toDropdown.sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
         waitFor(5000);
         //select the first item from the destination auto complete list
-        List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
+        List<WebElement> destinationOptions = toAutoFill.findElements(By.tagName("li"));
         destinationOptions.get(0).click();
+
+        // finding number of rows for dynamic calendar selection
         List<WebElement> listOfRows = driver.findElements(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr"));
         int noOfRows = listOfRows.size();
+
+        // finding number of columns for dynamic calendar selection
         List<WebElement> listOfCol = driver.findElements(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[" + noOfRows + "]/td[@data-event='click']"));
         int noOfCol = listOfCol.size();
+
         driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[" + noOfRows + "]/td[" + noOfCol + "]/a")).click();
 
         //all fields filled in. Now click on search
-        driver.findElement(By.id("SearchBtn")).click();
+        searchButton.click();
 
         waitFor(5000);
         //verify that result appears for the provided journey search
